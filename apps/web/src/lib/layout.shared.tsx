f95f537a0b4a0b9d4bfd9d42d58eab25ae039f58
atheeq-rhxn <atheeq.rhxn@gmail.com>
2025-10-26 22:07:12 +0530

    🔧 chore: add logo in docs

--- apps/web/src/lib/layout.shared.tsx
+++ apps/web/src/lib/layout.shared.tsx
@@ -1,9 +1,25 @@
+import Image from "next/image";
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+
+const logo = (
+	<Image
+		alt="MangoWC"
+		src="/logo-32x32.png"
+		width={32}
+		height={32}
+		className="size-5"
+	/>
+);
 
 export function baseOptions(): BaseLayoutProps {
 	return {
 		nav: {
-			title: "MangoWC",
+			title: (
+				<>
+					{logo}
+					<span className="font-medium max-md:hidden">MangoWC</span>
+				</>
+			),
 		},
 		githubUrl: "https://github.com/atheeq-rhxn/mangowc-docs",
 	};

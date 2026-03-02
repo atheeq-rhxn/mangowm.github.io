8a1d29bdb631504e1ab784b918cffa6d8d4eafa1
atheeq-rhxn <atheeq.rhxn@gmail.com>
2026-03-02 18:31:33 +0530

    chore: remove unnecessary files and update

--- apps/web/.source/source.config.mjs
+++ apps/web/.source/source.config.mjs
@@ -4,34 +4,11 @@ var docs = defineDocs({
   dir: "content/docs",
   docs: {
     postprocess: {
-      includeProcessedMarkdown: true,
-      extractLinkReferences: true
-    },
-    async: true
-  }
-});
-var source_config_default = defineConfig({
-  mdxOptions: {
-    rehypeCodeOptions: {
-      langs: [
-        "ts",
-        "js",
-        "html",
-        "tsx",
-        "mdx",
-        "ini",
-        "bash",
-        "nix",
-        "jsonc",
-        "css"
-      ],
-      themes: {
-        light: "catppuccin-latte",
-        dark: "catppuccin-mocha"
-      }
+      includeProcessedMarkdown: true
     }
   }
 });
+var source_config_default = defineConfig();
 export {
   source_config_default as default,
   docs
